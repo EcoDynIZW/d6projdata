0a7fd2448654d2416b39b0ef6b8edef7fd757d1e
wenzmo <wenzler@izw-berlin.de>
2021-04-16 17:47:57 +0200

    added an option for nas
    If you left something open (NA) you can change it no afterwards

--- R/build_data_files.R
+++ R/build_data_files.R
@@ -9,6 +9,8 @@
 
 
 build_data_files <- function(path = "."){
+
+
   # function for dates
   fun_date <- function(x){
     date_in <- base::readline(base::paste0("enter date ", x,": "))
@@ -45,59 +47,105 @@ build_data_files <- function(path = "."){
     return(epsg_in)
   }
 
+  data <- dplyr::tibble(proj_id = NA,
+                        proj_editor = NA,
+                        Dep = NA,
+                        sup_vis = NA,
+                        data_name = NA,
+                        data_name_archive = NA,
+                        edited_unique_identifier = NA,
+                        original_unique_identifier = NA,
+                        storage_folder = NA,
+                        study_country = NA,
+                        study_area = NA,
+                        species = NA,
+                        type_of_sample = NA,
+                        type_of_data = NA,
+                        type_of_measure = NA,
+                        repeated_measure = NA,
+                        no_spec = NA,
+                        no_ind = NA,
+                        data_from = NA,
+                        data_to = NA,
+                        additional_info = NA,
+                        publication = NA,
+                        publication_doi = NA,
+                        metadata_description = NA,
+                        temporal_resolution_sec = NA,
+                        temporal_resolution_min = NA,
+                        IZW_archive_path = NA,
+                        IZW_data_path = NA,
+                        external_archives = NA,
+                        sample_data = NA,
+                        proc_data = NA,
+                        cooperation = NA,
+                        data_owner = NA,
+                        projection = NA,
+                        EPSG_code = NA,
+                        data_type = NA)
 
-  data <- dplyr::tibble(proj_id = base::readline("project id:"),
-                        proj_editor = base::readline("project editor:"),
-                        Dep = fun_num("Department"),
-                        sup_vis = base::readline("super visor:"),
-                        data_name = base::readline("names of data files:"),
-                        data_name_archive = base::readline("original names of data files in archive:"),
-                        edited_unique_identifier = base::readline("edited unique identifier:"),
-                        original_unique_identifier = base::readline("original unique identifier:"),
-                        storage_folder = "data-raw",
-                        study_country = base::readline("study country:"),
-                        study_area = base::tolower(base::readline("study area:")),
-                        species = base::tolower(base::readline("species:")),
-                        type_of_sample = base::readline("type of sample:"),
-                        type_of_data = base::readline("type of data:"),
-                        type_of_measure = base::readline("type of measure:"),
-                        repeated_measure = c("yes", "no")[utils::menu(c("yes", "no"), title = "repeated measure?")],
-                        no_spec = fun_num("number of species"),
-                        no_ind = fun_num("number of individuals"),
-                        data_from = fun_date("from"),
-                        data_to = fun_date("to"),
-                        additional_info = base::readline("additional information:"),
-                        publication = base::readline("publication name:"),
-                        publication_doi = base::readline("publication doi:"),
-                        metadata_description = base::readline("metadata description:"),
-                        temporal_resolution_sec = fun_num("temporal resolution sec"),
-                        temporal_resolution_min = fun_num("temporal resolution min"),
-                        IZW_archive_path = base::readline("IZW archive path:"),
-                        IZW_data_path = base::readline("IZW data path:"),
-                        external_archives = base::readline("external archives:"),
-                        sample_data = base::readline("sample data:"),
-                        proc_data = base::readline("processed data:"),
-                        cooperation = base::readline("cooperation:"),
-                        data_owner = base::readline("data owner:"),
-                        projection = base::readline("projection name:"),
-                        EPSG_code = fun_epsg(),
-                        data_type = base::readline("data type ending:"))
+  doit <- "Yes"
 
-  data2 <- dplyr::mutate(data,
+  while(doit == "Yes"){
+
+    data <- data %>%
+      dplyr::mutate(
+        proj_id = ifelse(is.na(data$proj_id), base::readline("project id:"), data$proj_id),
+        proj_editor = ifelse(is.na(data$proj_editor), base::readline("project editor:"), data$proj_editor),
+        Dep = ifelse(is.na(data$Dep), fun_num("Department"), data$Dep),
+        sup_vis = ifelse(is.na(data$sup_vis), base::readline("super visor:"), data$sup_vis),
+        data_name = ifelse(is.na(data$data_name), base::readline("names of data files:"),data$data_name),
+        data_name_archive = ifelse(is.na(data$data_name_archive), base::readline("original names of data files in archive:"), data$data_name_archive),
+        edited_unique_identifier = ifelse(is.na(data$edited_unique_identifier), base::readline("edited unique identifier:"), data$edited_unique_identifier),
+        original_unique_identifier = ifelse(is.na(data$original_unique_identifier), base::readline("original unique identifier:"), data$original_unique_identifier),
+        storage_folder = "data-raw",
+        study_country = ifelse(is.na(data$study_country), base::readline("study country:"), data$study_country),
+        study_area = ifelse(is.na(data$study_area), base::tolower(base::readline("study area:")), data$study_area),
+        species = ifelse(is.na(data$species), base::tolower(base::readline("species:")), data$species),
+        type_of_sample = ifelse(is.na(data$type_of_sample), base::readline("type of sample:"), data$type_of_sample),
+        type_of_data = ifelse(is.na(data$type_of_data), base::readline("type of data:"), data$type_of_data),
+        type_of_measure = ifelse(is.na(data$type_of_measure), base::readline("type of measure:"), data$type_of_measure),
+        repeated_measure = c("yes", "no")[utils::menu(c("yes", "no"), title = "repeated measure?")],
+        no_spec = ifelse(is.na(data$no_spec), fun_num("number of species"), data$no_spec),
+        no_ind = ifelse(is.na(data$no_ind), fun_num("number of individuals"), data$no_ind),
+        data_from = ifelse(is.na(data$data_from), fun_date("from"), data$data_from),
+        data_to = ifelse(is.na(data$data_to), fun_date("to"), data$data_to),
+        additional_info = ifelse(is.na(data$additional_info), base::readline("additional information:"), data$additional_info),
+        publication = ifelse(is.na(data$publication), base::readline("publication name:"), data$publication),
+        publication_doi = ifelse(is.na(data$publication_doi), base::readline("publication doi:"), data$publication_doi),
+        metadata_description = ifelse(is.na(data$metadata_description), base::readline("metadata description:"), data$metadata_description),
+        temporal_resolution_sec = ifelse(is.na(data$temporal_resolution_sec), fun_num("temporal resolution sec"), data$temporal_resolution_sec),
+        temporal_resolution_min = ifelse(is.na(data$temporal_resolution_min), fun_num("temporal resolution min"), data$temporal_resolution_min),
+        IZW_archive_path = ifelse(is.na(data$IZW_archive_path), base::readline("IZW archive path:"), data$IZW_archive_path),
+        IZW_data_path = ifelse(is.na(data$IZW_data_path), base::readline("IZW data path:"), data$IZW_data_path),
+        external_archives = ifelse(is.na(data$external_archives), base::readline("external archives:"), data$external_archives),
+        sample_data = ifelse(is.na(data$sample_data), base::readline("sample data:"), data$sample_data),
+        proc_data = ifelse(is.na(data$proc_data), base::readline("processed data:"), data$proc_data),
+        cooperation = ifelse(is.na(data$cooperation), base::readline("cooperation:"), data$cooperation),
+        data_owner = ifelse(is.na(data$data_owner), base::readline("data owner:"), data$data_owner),
+        projection = ifelse(is.na(data$projection), base::readline("projection name:"), data$projection),
+        EPSG_code = ifelse(is.na(data$EPSG_code), fun_epsg(), data$EPSG_code),
+        data_type = ifelse(is.na(data$data_type), base::readline("data type ending:"), data$data_type)
+      ) %>%
+      dplyr::mutate_each(dplyr::funs(empty_as_na))
+
+  data <- dplyr::mutate(data,
                          proj_name = base::paste(lubridate::year(data_from),
                                                  species,
                                                  study_country,
                                                  study_area,
                                                  type_of_data,
                                                  proj_editor,
-                                                 sep = "_")) %>%
-    dplyr::mutate_each(dplyr::funs(empty_as_na))
+                                                 sep = "_"))
 
-  if(!base::file.exists(base::paste(path, data2$proj_name, sep = "/"))){ # creates new folder per layer
-    base::dir.create(base::paste(path, data2$proj_name, sep = "/"))
+  doit <- c("Yes", "No")[utils::menu(c("Yes", "No"), title = "Do you want to change something?")]
   }
 
-xlsx::write.xlsx(data2, base::paste0(path, "/", data2$proj_name, "/", data2$proj_name, ".xlsx"),
+  if(!base::file.exists(base::paste(path, data$proj_name, sep = "/"))){ # creates new folder per layer
+    base::dir.create(base::paste(path, data$proj_name, sep = "/"))
+  }
+
+xlsx::write.xlsx(data, base::paste0(path, "/", data$proj_name, "/", data$proj_name, ".xlsx"),
                  row.names = FALSE)
 
 }
