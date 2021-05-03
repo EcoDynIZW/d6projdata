1aa430f0f17da0aa35ee64166d211840076e5759
wenzmo <wenzler@izw-berlin.de>
2021-05-03 20:46:32 +0200

    converted all columns to character for plotting

--- R/get_meta_info.R
+++ R/get_meta_info.R
@@ -18,6 +18,7 @@ get_meta_info <- function(path){
       dplyr::select(proj_id, proj_editor, Dep, sup_vis, species,
                     type_of_sample, type_of_data, type_of_measure,
                     no_spec, no_ind, publication, publication_doi) %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(), as.character)) %>%
       tidyr::pivot_longer(cols = c("proj_id", "proj_editor", "Dep", "sup_vis",
                                    "species", "type_of_sample", "type_of_data",
                                    "type_of_measure", "no_spec", "no_ind",
