d8b62838f7e67cb9d8b2122c6e09bfe3a7480fe9
wenzmo <wenzler@izw-berlin.de>
2022-06-01 13:22:15 +0200

    created render project data function

---
+++ R/render_projdata.R
@@ -0,0 +1,16 @@
+#' function for rendering an markdown to get an html of te projectdata
+#' @param path The path where the data has to be stored
+#' @return a tibble where you have to set the parameters by hand.
+#' @export
+#' @examples
+#' \dontrun{
+#' render_geodata()
+#' }
+
+render_projdata <-
+  function(path, data_name, data_sf, proj_data) {
+    rmarkdown::render(here::here("R", "projdata_template_html.Rmd"),
+                      output_format = "html_document",
+                      output_dir = path,
+                      output_file = unlist(stringi::stri_split(path, regex = "/"))[length(unlist(stringi::stri_split(path, regex = "/")))])
+  }
