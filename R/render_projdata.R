a14d7b69d07f90f9eece01bbc0137e2a32580df9
wenzmo <wenzler@izw-berlin.de>
2022-06-01 13:30:13 +0200

    fixed typos

--- R/render_projdata.R
+++ R/render_projdata.R
@@ -1,10 +1,10 @@
-#' function for rendering an markdown to get an html of te projectdata
+#' function for rendering an markdown to get an html of the project data
 #' @param path The path where the data has to be stored
 #' @return a tibble where you have to set the parameters by hand.
 #' @export
 #' @examples
 #' \dontrun{
-#' render_geodata()
+#' render_projdata()
 #' }
 
 render_projdata <-
