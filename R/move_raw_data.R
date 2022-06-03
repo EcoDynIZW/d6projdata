f39509298e2c0d434e46ef82e137a1355c31a641
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2022-06-03 12:13:09 +0200

    renewed fun move raw function

--- R/move_raw_data.R
+++ R/move_raw_data.R
@@ -1,5 +1,5 @@
 #' move raw data from any directoy to the _archive folder.
-#' @param taxon_col column of the taxon name
+#' @param data_name column of the taxon name
 #' @param data_path complete path to the raw data with the file itself
 #' @export
 #' @examples
@@ -7,22 +7,14 @@
 #' fun_move_raw(taxon_col = "taxon_col", data_path = "path")
 #' }
 
-fun_move_raw <- function(taxon_col, data_path){
+fun_move_raw <- function(data_name, data_path){
 
-  stud_spec <- base::gsub(pattern = " ",
-                    replacement = "_",
-                    x = base::tolower(taxon_col))
-
-  dir <- here::here("data-raw",
-                    base::list.files(path = here::here("data-raw"),
-                         pattern = stud_spec))
-
-  if (!base::dir.exists(paths = base::paste0(dir, "/_archive")))
-    base::dir.create(path = base::paste0(dir, "/_archive"),
+  if(!base::dir.exists(paths = here::here("data-raw", data_name, "/_archive")))
+    base::dir.create(path = here::here("data-raw", data_name, "/_archive"),
                      showWarnings = TRUE,
                      recursive = TRUE)
 
   base::file.copy(from = data_path,
-                  to = base::paste0(dir, "/_archive"))
+                  to = here::here("data-raw", data_name, "/_archive"))
 
 }
